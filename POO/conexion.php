c1481621679f9da9b0bce623fb8f45c772b82ba2
Mauro Salina <maurosalina85@gmail.com>
2025-01-21 14:11:03 -0300

    guardando por las dudas los ultimos cambios

---
+++ POO/conexion.php
@@ -0,0 +1,18 @@
+<?php 
+require "config.php";
+
+class Conexion {
+
+    protected $conexion;
+
+    public function __construct()
+    {
+        //aca va la conexion con PDO usando try catch
+    }
+
+}
+
+
+
+
+?>
---
+++ POO/config.php
@@ -0,0 +1,8 @@
+<?php 
+
+define("DB_HOST", "localhost");
+define("DB_DATABASE", "usuarios");
+define("DB_USER", "root");
+define("DB_PASS", "");
+
+?>
